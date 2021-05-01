13405c589e2a798f71cc1ab014ba97dbb499a6d5
Joseph <josephtsegen10@gmail.com>
2021-05-01 22:21:34 +0100

    nothing to see here

--- nipro/settings.py
+++ nipro/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$27ilg16taqu3s#w+wexq8436iqq9y&1)8c@s)#0!j0-yr@lxw'
+SECRET_KEY = env("SECRET_KEY") #top secret haha
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
