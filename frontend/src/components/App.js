ce3d73adc9907a34fcac59985220c1b6b121d9b8
Joseph <josephtsegen10@gmail.com>
2021-05-08 02:13:38 +0100

    added nova fonts

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,7 +4,7 @@ import { render } from "react-dom";
 export default function App() {
   return (
     <div>
-      <h1>Nipro</h1>
+      <h1 id="nipro-logo">Nipro</h1>
       <p>construction in progress......</p>
       <p>It appears to work</p>
       <p>let's start working</p>
