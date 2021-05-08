56c579757619fc5c2c6a2b963a351e6ec3a86a4d
Joseph <josephtsegen10@gmail.com>
2021-05-08 03:45:38 +0100

    made it look like a standard create-react-app boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1 id="nipro-logo">Nipro</h1>
+      <p>construction in progress......</p>
+    </div>
+  );
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,6 @@
-import App from "./components/App.jsx";
+import React from "react";
+import { render } from "react-dom";
+import App from "./App.jsx";
 import "./index.css";
+
+render(<App />, document.getElementById("app"));
