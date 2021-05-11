5db9f2207885983134a33b42bc806201c9bc63e5
Joseph <josephtsegen10@gmail.com>
2021-05-11 17:00:44 +0100

    writting some additional info in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,21 @@ module.exports = {
   output: {
     // where compiled files go
     path: path.resolve(__dirname, "./nipro/frontend/static/frontend/"),
-    // 127.0.0.1/static/frontend/public/ where files are served from
-    publicPath: "./static/frontend/",
-    filename: "bundle.js",
+    // (127.0.0.1/static/frontend/ where files are served from)
+    publicPath: "./static/frontend/", // without this line webpack serve will not compile the bundle.js
+    filename: "bundle.js", // this is the name of the compiled file
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|mjs)$/,
+        test: /\.(js|jsx|mjs)$/, // this allows files to be saved as either .js , .jsx or .mjs
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: "babel-loader",
         },
       },
       {
+        // this allows css to be written and imported in the react folders
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
