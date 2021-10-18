1c6bd37a70702c6271c9d8d4e3929b425b31da66
joseph <josephtsegen10@gmail.com>
2021-10-18 04:09:22 +0100

    added material icons

--- nitro/src/components/NavBar.jsx
+++ nitro/src/components/NavBar.jsx
@@ -1,20 +1,25 @@
-//@ts-check
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 const NavBar = () => {
   return (
-    <nav className="nav-bar">
-      <label>nipro</label>
-
-      <div className="nav-buttons">
-        <Button href="/" variant="text" color="primary">
-          Home
-        </Button>
-        <Button href="/login" variant="text" color="primary">
-          Login
-        </Button>
-      </div>
-    </nav>
+    <div>
+      <AppBar color="secondary" position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            nipro
+          </Typography>
+        </Toolbar>
+        <Toolbar>
+          <Button href="/" variant="text" color="error">
+            {" "}
+            Home
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
